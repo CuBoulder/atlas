4843b1e49258996e6b0dd7906ed6d64c3b6691f1
James W Fuller <James.W.Fuller@Colorado.EDU>
2016-06-21 14:50:11 -0600

    Rewrite server definitions so that we can call them from Fabric correctly. Use full words for environments.

--- config_servers.py
+++ config_servers.py
@@ -3,50 +3,64 @@ Server configuration file for Atlas
 
 All server definitions go here.
 """
-platforms = {
-    'poola-custom': {
-        'prod': ['wweb1', 'wweb2'],
-        'test': ['wwebtest1', 'wwebtest2'],
-        'stage': ['wstage1'],
-        'dev': ['wwebdev1'],
+# Servers manipulate using Fabric.
+roledefs = {
+    'production': {
+        'webservers' : ['wweb3.int.colorado.edu', 'wweb4.int.colorado.edu', 'wweb5.int.colorado.edu'],
+        'varnish_servers': ['172.20.62.71', '172.20.62.72'],
+        'memcache_servers': ['wmem1.int.colorado.edu:11212', 'wmem2.int.colorado.edu:11212'],
+        'f5_servers': ['its4-f5.colorado.edu', 'its5-f5.colorado.edu'],
     },
-    'poolb-express': {
-        'prod': ['wweb3', 'wweb4', 'wweb5'],
-        'test': ['wwebtest3', 'wwebtest4', 'wwebtest5'],
-        'dev': ['wwebdev2'],
-        'local': ['express.local'],
+    'local': {
+        'webservers': ['express.local'],
+        'varnish_servers': ['localhost'],
+        'memcache_servers': ['localhost:11211'],
+        'f5_servers': [],
     },
-    'poolb-homepage': {
-        'prod': ['wweb3', 'wweb4', 'wweb5'],
-        'test': ['wwebtest3', 'wwebtest4', 'wwebtest5'],
-        'dev': ['wwebdev2'],
-        'local': ['express.local'],
+    'webservers': {
+        'test': ['wwebtest3.int.colorado.edu', 'wwebtest4.int.colorado.edu', 'wwebtest5.int.colorado.edu'],
+        'development': ['wwebdevelopment2.int.colorado.edu'],
     },
+    'varnish_servers': {
+        'test': ['172.20.62.41', '172.20.62.42', ],
+        'development': ['172.20.62.11', ],
+    },
+    'memcache_servers': {
+        'test': ['wmemtest1.int.colorado.edu:11212', 'wmemtest2.int.colorado.edu:11212', ],
+        'development': ['wmemdevelopment1.int.colorado.edu:11212', ],
+    },
+    'f5_servers': {
+        'test': ['uctool@its6-f5.colorado.edu', 'uctool@its7-f5.colorado.edu'],
+        'development': ['uctool@its6-f5.colorado.edu', 'uctool@its7-f5.colorado.edu'],
+    }
+}
+
+varnish_control_terminals = {
+    'production': 'wvarn3.int.colorado.edu:6082 wvarn4.int.colorado.edu:6082',
+    'stage': 'wstage1.colorado.edu:6082',
+    'test': 'wvarntest3.int.colorado.edu:6082 wvarntest4.int.colorado.edu:6082',
+    'development': 'wvarndevelopment2.int.colorado.edu:6082',
+    'local': 'localhost:6082',
+}
+
+nfs_mount_location = {
+    'production': '/Net/hanfs/wwwng-poolb',
+    'test': '/Net/hanfs-test/wwwng-poolb',
+    'development': '/Net/hanfs-test/wwwng-poolb',
+    'local': '/wwwng',
 }
 
 base_urls = {
-    'prod': 'http://www.colorado.edu',
+    'production': 'http://www.colorado.edu',
     'test': 'http://www-test.colorado.edu',
-    'stage': 'http://www-stage.colorado.edu',
-    'dev': 'http://www-dev.colorado.edu',
+    'development': 'http://www-development.colorado.edu',
     'local': 'http://express.local',
 }
 
-# Fabric uses this and the environment variable to determine which F5 servers
-# to update. DEV and TEST share an f5, but have separate config files on the
-# servers.
-f5_servers = {
-    'prod': ['uctool@its4-f5.colorado.edu', 'uctool@its5-f5.colorado.edu'],
-    'test': ['uctool@its6-f5.colorado.edu', 'uctool@its7-f5.colorado.edu'],
-    'dev': ['uctool@its6-f5.colorado.edu', 'uctool@its7-f5.colorado.edu'],
-    'local': [],
-}
-
-# f5 conf filename per environment.
 f5_config_files = {
-    'prod': 'WWWNGProdDataGroup.dat',
+    'production': 'WWWNGproductionDataGroup.dat',
     'test': 'WWWNGTestDataGroup.dat',
-    'dev': 'WWWNGDevDataGroup.dat',
+    'development': 'WWWNGdevelopmentDataGroup.dat',
 }
 # Entries in the f5 that are not sites.
 f5_exceptions = [
@@ -56,30 +70,3 @@ f5_exceptions = [
     '/p1',
 ]
 
-# Varnish server IP addresses.
-varnish_servers = {
-    'prod': ['172.20.62.71', '172.20.62.72', ],
-    'stage': ['128.138.128.61', ],
-    'test': ['172.20.62.41', '172.20.62.42', ],
-    'dev': ['172.20.62.11', ],
-    'local': ['localhost'],
-}
-
-# Varnish control terminals. Separate multiple for a given environment with a
-# space.
-varnish_control_terminals = {
-    'prod': 'wvarn3.int.colorado.edu:6082 wvarn4.int.colorado.edu:6082',
-    'stage': 'wstage1.colorado.edu:6082',
-    'test': 'wvarntest3.int.colorado.edu:6082 wvarntest4.int.colorado.edu:6082',
-    'dev': 'wvarndev2.int.colorado.edu:6082',
-    'local': 'localhost:6082',
-}
-
-# Memcache servers.
-memcache_servers = {
-    'prod': ['wmem1.int.colorado.edu:11212', 'wmem2.int.colorado.edu:11212', ],
-    'stage': ['wstage1.colorado.edu:11211', ],
-    'test': ['wmemtest1.int.colorado.edu:11212', 'wmemtest2.int.colorado.edu:11212', ],
-    'dev': ['wmemdev1.int.colorado.edu:11211', ],
-    'local': ['localhost:11211'],
-}

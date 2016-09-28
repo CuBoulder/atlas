9e9a8c7737ade932033f443146281a3a3b52cdee
James W Fuller <James.W.Fuller@Colorado.EDU>
2016-09-28 08:30:05 -0600

    Added Statistics resource and added a reference to Site.

--- config_data_structure.py
+++ config_data_structure.py
@@ -180,6 +180,14 @@ sites_schema = {
             },
         },
     },
+    'statistics': {
+        'type': 'objectid',
+        'data_relation': {
+            'resource': 'statistics',
+            'field': '_id',
+            'embeddable': True,
+        },
+    },
 }
 
 commands_schema = {
@@ -205,6 +213,29 @@ commands_schema = {
     },
 }
 
+statistics_schema = {
+    'statistics': {
+        'type': 'list',
+        'nullable': True,
+        'schema': {
+            'type': 'dict',
+            'allow_unknown': True,
+            'schema': {
+                'var_theme_default': {'type': 'string'},
+                'var_ga_account': {'type': 'string'},
+                'var_livechat_license_number': {'type': 'string'},
+                'var_inactive_30_email': {'type': 'boolean'},
+                'var_inactive_55_email': {'type': 'boolean'},
+                'var_inactive_60_email': {'type': 'boolean'},
+                'var_cron_last': {'type': 'integer'},
+                'num_days_since_last_edit': {'type': 'integer'},
+                'num_nodes': {'type': 'integer'},
+                'num_beans': {'type': 'integer'},
+            },
+        },
+    },
+}
+
 """
 Definitions of Resources.
 Tells Eve what methods and schemas apply to a given resource.
@@ -227,12 +258,22 @@ sites = {
         'url': 'regex("[\w]+")',
         'field': 'sid'
     },
-    'embedded_fields': ['code.core','code.profile','code.package'],
+    'embedded_fields': ['code.core', 'code.profile', 'code.package', 'statistics'],
     'public_methods': ['GET'],
     'public_item_methods': ['GET'],
     'versioning': True,
     'soft_delete': True,
     'schema': sites_schema,
+}
+
+# Statistics resource
+statistics = {
+    'item_title': 'stats',
+    'public_methods': ['GET'],
+    'public_item_methods': ['GET'],
+    'versioning': True,
+    'soft_delete': True,
+    'schema': statistics_schema,
 }
 
 # Command resource

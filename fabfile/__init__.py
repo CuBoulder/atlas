9fb1a0587daae7140c6a28718a07efa2b0467465
James W Fuller <James.W.Fuller@Colorado.EDU>
2017-01-09 09:50:37 -0700

    Add password to use when chowning files.

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -25,6 +25,8 @@ jinja_env = Environment(loader=PackageLoader('atlas', 'templates'))
 
 # Fabric environmental settings.
 env.user = ssh_user
+# TODO: Remove this after migration
+env.password = ssh_user_password
 # env.key_filename =
 
 # Allow ~/.ssh/config to be utilized.

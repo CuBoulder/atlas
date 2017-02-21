438b31ef39068e463b8c8b5cb49dec0f48d0e20a
James W Fuller <James.W.Fuller@Colorado.EDU>
2017-02-21 08:37:47 -0700

    Deal with var conditionally.

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -597,8 +597,14 @@ def _create_settings_files(site, profile_name):
     status = site['status']
     id = site['_id']
     statistics = site['statistics']
-    siteimprove_site = site['settings']['siteimprove_site']
-    siteimprove_group = site['settings']['siteimprove_group']
+    if site['settings'].get('siteimprove_site'):
+        siteimprove_site = site['settings']['siteimprove_site']
+    else:
+        siteimprove_site = None
+    if site['settings'].get('siteimprove_group'):
+        siteimprove_group = site['settings']['siteimprove_group']
+    else:
+        siteimprove_group = None
     page_cache_maximum_age = site['settings']['page_cache_maximum_age']
     atlas_url = '{0}/'.format(api_urls[environment])
     database_password = utilities.decrypt_string(site['db_key'])

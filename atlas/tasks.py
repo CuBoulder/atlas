05f296915f93e1be6919bc32dd8e5e759d1183fe
James W Fuller <James.W.Fuller@Colorado.EDU>
2017-11-22 11:22:09 -0700

    Resolves #95.

--- atlas/tasks.py
+++ atlas/tasks.py
@@ -267,7 +267,6 @@ def code_remove(item):
         # Slack notification
         slack_title = 'Code Remove - Success'
         slack_color = 'good'
-        code_string = '{0} - {1}'.format(item['meta']['name'], item['meta']['version'])
 
         slack_payload = {
             "text": slack_title,
@@ -289,12 +288,12 @@ def code_remove(item):
                         },
                         {
                             "title": "Name",
-                            "value": name,
+                            "value": item['meta']['name'],
                             "short": True
                         },
                         {
                             "title": "Version",
-                            "value": version,
+                            "value": item['meta']['version'],
                             "short": True
                         }
                     ],
@@ -856,6 +855,30 @@ def delete_all_available_sites():
         for site in sites['_items']:
             log.debug('Delete all available sites| Site - %s', site)
             utilities.delete_eve('sites', site['_id'])
+
+
+@celery.task
+def remove_unused_code():
+    """
+    If a code item is more than 60 days old, not current, and unused then remove it.
+    """
+    time_ago = datetime.utcnow() - timedelta(days=90)
+    code_query = 'where={{"meta.is_current":false,"_created":{{"$lte":"{0}"}}}}'.format(time_ago.strftime("%Y-%m-%d %H:%M:%S GMT"))
+    code_items = utilities.get_eve('code', code_query)
+
+    for code in code_items['_items']:
+        # Check for sites using this piece of code.
+        if code['meta']['code_type'] in ['module', 'theme', 'library']:
+            code_type = 'package'
+        else:
+            code_type = code['meta']['code_type']
+        log.debug('code | Check unused | code - %s | code_type - %s', code['_id'], code_type)
+        site_query = 'where={{"code.{0}":"{1}"}}'.format(code_type, code['_id'])
+        sites = utilities.get_eve('sites', site_query)
+        log.debug('code | Delete | code - %s | sites result - %s', code['_id'], sites)
+        if sites['_meta']['total'] == 0:
+            log.info('code | Removing unused item | code - %s', code['_id'])
+            utilities.delete_eve('code', code['_id'])
 
 
 @celery.task

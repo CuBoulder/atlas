255863e544c1709bcf8b1c3a581d749ce2010e2e
James W Fuller <James.W.Fuller@Colorado.EDU>
2018-02-01 14:44:19 -0700

    Cookie domain.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ Resolves:
 - &#35;437 Remove commands posting to Slack
 - &#35;408 Get timestamp into atlas.log
 - &#35;438 Updating the current and label meta data for a code asset should not trigger a code-re-add process
+- &#35;441 Add a cookie domain to settings file
 
 ## v2.1.0
 
--- atlas/templates/settings.php
+++ atlas/templates/settings.php
@@ -110,21 +110,31 @@ if (isset($_SERVER["WWWNG_ENV"]) || PHP_SAPI === "cli") {
   if (isset($_SERVER['WWWNG_ENV'])) {
     global $base_url;
 
+    /**
+     * Drupal automatically generates a unique session cookie name for each site
+     * based on its full domain name.Since we want different cookies per
+     * environment, we need to specify that here. Make sure to always start the
+     * $cookie_domain with a leading dot, as per RFC 2109.
+     */
     switch($_SERVER['WWWNG_ENV']) {
       case 'cust_dev':
         $base_url .= 'https://www-dev.colorado.edu';
+        $cookie_domain = '.www-dev.colorado.edu';
         break;
 
       case 'cust_test':
         $base_url .= 'https://www-test.colorado.edu';
+        $cookie_domain = '.www-test.colorado.edu';
         break;
 
       case 'cust_prod':
         $base_url .= 'https://www.colorado.edu';
+        $cookie_domain = '.www.colorado.edu';
         break;
 
       case 'express_local':
         $base_url .= 'https://express.local';
+        // We don't need a cookie_domain for locals.
         break;
 
     }

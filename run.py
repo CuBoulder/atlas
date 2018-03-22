4ec9d7910531dac0280d21f5de758b73864e0801
James W Fuller <James.W.Fuller@Colorado.EDU>
2018-03-22 08:10:33 -0600

    Correct format.

--- run.py
+++ run.py
@@ -120,7 +120,7 @@ def f5():
         if 'path' in site:
             # In case a path was saved with a leading slash
             path = site["path"] if site["path"][0] == '/' else '/' + site["path"]
-            f5_list.append('"{0}" := "legacy"'.format(path))
+            f5_list.append('"/{0}" := "legacy",'.format(path))
     response = make_response(','.join(f5_list))
     return response
 

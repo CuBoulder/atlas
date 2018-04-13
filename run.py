96f4f8f7839d3a4b41bd91984d8f73399e0bab6e
James W Fuller <James.W.Fuller@Colorado.EDU>
2018-04-13 10:44:29 -0600

    We take all the files. And we don't show download URLs since they don't work

--- run.py
+++ run.py
@@ -105,21 +105,6 @@ def restore_backup(backup_id):
     return response
 
 
-@app.route('/backup/<string:backup_id>/download', methods=['GET'])
-# TODO: Test what happens with 404 for backup_id
-@requires_auth('backup')
-def download_backup(backup_id):
-    """
-    Return URLs to download the database and files
-    """
-    app.logger.info('Backup | Download | ID - %s', backup_id)
-    backup_record = utilities.get_single_eve('backup', backup_id)
-    app.logger.debug('Backup | Download | Backup record - %s', backup_record)
-    urls = {'files' : '{0}/download/{1}'.format(API_URLS[ENVIRONMENT], backup_record['files'])}
-    urls['db'] = '{0}/download/{1}'.format(API_URLS[ENVIRONMENT], backup_record['database'])
-    return jsonify(result=urls)
-
-
 @app.route('/sites/<string:site_id>/backup', methods=['POST'])
 # TODO: Test what happens with 404 for site_id
 @requires_auth('backup')

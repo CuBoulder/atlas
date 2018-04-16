0a28a8d4e96a7f0556a646c6442e74e8da7207c7
James W Fuller <James.W.Fuller@Colorado.EDU>
2018-04-16 11:58:35 -0600

    Correct var.

--- atlas/tasks.py
+++ atlas/tasks.py
@@ -1188,6 +1188,6 @@ def import_backup(env, backup_id, target_instance):
     log.info('Import Backup | Backup - %s', backup)
     target = utilities.get_single_eve('sites', target_instance)
     host = utilities.single_host()
-    utilities.create_database(instance['sid'], instance['db_key'])
+    utilities.create_database(target['sid'], target['db_key'])
     execute(fabric_tasks.instance_heal, item=target)
     execute(fabric_tasks.import_backup, backup=backup.json(), target_instance=target, hosts=host)

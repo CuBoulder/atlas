c0b5b32a30cafbd2b19eef9dbfe434a11c16fa60
James W Fuller <James.W.Fuller@Colorado.EDU>
2018-04-16 08:42:32 -0600

    Comment out cron tasks for now.

--- atlas/config_celery.py
+++ atlas/config_celery.py
@@ -71,53 +71,53 @@ CELERY_ROUTES = {
 }
 
 CELERYBEAT_SCHEDULE = {
-    'launched_cron': {
-        'task': 'atlas.tasks.cron',
-        'schedule': timedelta(minutes=60),
-        'kwargs': {
-            "status": "launched",
-        },
-    },
-    'locked_cron': {
-        'task': 'atlas.tasks.cron',
-        'schedule': timedelta(hours=6),
-        'kwargs': {
-            "status": "locked",
-        },
-    },
-    'installed_cron': {
-        'task': 'atlas.tasks.cron',
-        'schedule': timedelta(hours=3),
-        'kwargs': {
-            "status": "installed",
-        },
-    },
-    'available_sites_check': {
-        'task': 'atlas.tasks.available_sites_check',
-        'schedule': timedelta(minutes=5),
-    },
+    # 'launched_cron': {
+    #     'task': 'atlas.tasks.cron',
+    #     'schedule': timedelta(minutes=60),
+    #     'kwargs': {
+    #         "status": "launched",
+    #     },
+    # },
+    # 'locked_cron': {
+    #     'task': 'atlas.tasks.cron',
+    #     'schedule': timedelta(hours=6),
+    #     'kwargs': {
+    #         "status": "locked",
+    #     },
+    # },
+    # 'installed_cron': {
+    #     'task': 'atlas.tasks.cron',
+    #     'schedule': timedelta(hours=3),
+    #     'kwargs': {
+    #         "status": "installed",
+    #     },
+    # },
+    # 'available_sites_check': {
+    #     'task': 'atlas.tasks.available_sites_check',
+    #     'schedule': timedelta(minutes=5),
+    # },
     'delete_stuck_pending_sites': {
         'task': 'atlas.tasks.delete_stuck_pending_sites',
         'schedule': timedelta(minutes=5),
     },
-    'remove_orphan_statistics': {
-        'task': 'atlas.tasks.remove_orphan_statistics',
-        'schedule': timedelta(minutes=60),
-    },
-    'remove_stale_installed_sites': {
-        'task': 'atlas.tasks.take_down_installed_old_sites',
-        'schedule': crontab(minute=0, hour=2),
-    },
-    'verify_statistics_updating': {
-        'task': 'atlas.tasks.verify_statistics',
-        'schedule': timedelta(hours=24),
-    },
-    'remove_unused_code': {
-        'task': 'atlas.tasks.remove_unused_code',
-        'schedule': timedelta(hours=24),
-    },
-    'remove_old_backups': {
-        'task': 'atlas.tasks.remove_old_backups',
-        'schedule': timedelta(hours=24),
-    },
+    # 'remove_orphan_statistics': {
+    #     'task': 'atlas.tasks.remove_orphan_statistics',
+    #     'schedule': timedelta(minutes=60),
+    # },
+    # 'remove_stale_installed_sites': {
+    #     'task': 'atlas.tasks.take_down_installed_old_sites',
+    #     'schedule': crontab(minute=0, hour=2),
+    # },
+    # 'verify_statistics_updating': {
+    #     'task': 'atlas.tasks.verify_statistics',
+    #     'schedule': timedelta(hours=24),
+    # },
+    # 'remove_unused_code': {
+    #     'task': 'atlas.tasks.remove_unused_code',
+    #     'schedule': timedelta(hours=24),
+    # },
+    # 'remove_old_backups': {
+    #     'task': 'atlas.tasks.remove_old_backups',
+    #     'schedule': timedelta(hours=24),
+    # },
 }

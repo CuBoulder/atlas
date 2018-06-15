8caa6eb36c882c0aa099137b5c7cc73334a03e85
James W Fuller <James.W.Fuller@Colorado.EDU>
2018-06-15 10:12:22 -0600

    Corrected directory permission that prevented clean removal of instances.

--- atlas/fabric_tasks.py
+++ atlas/fabric_tasks.py
@@ -362,9 +362,9 @@ def site_remove(site):
     web_directory_path = '{0}/{1}'.format(SITES_WEB_ROOT, site['path'])
 
     # Fix perms to allow settings file to be removed.
-    settings_file = "{0}/{1}/{1}/sites/default/settings.php".format(SITES_CODE_ROOT, site['sid'])
-    if exists(settings_file):
-        run("chmod u+w {0}".format(settings_file))
+    sites_dir = "{0}/{1}/{1}/sites".format(SITES_CODE_ROOT, site['sid'])
+    if exists(sites_dir):
+        run("chmod -R u+w {0}".format(sites_dir))
 
     remove_symlink(web_directory)
     remove_symlink(web_directory_path)

7f5efa0a459595a7c2450e444f7ad48e2a6dec0c
James W Fuller <James.W.Fuller@Colorado.EDU>
2018-06-21 09:12:42 -0600

    Crontabs require minutes, or they run once per minute for the full hour.

--- atlas/config_celery.py
+++ atlas/config_celery.py
@@ -126,15 +126,15 @@ CELERYBEAT_SCHEDULE = {
     },
     'routine_backups': {
         'task': 'atlas.tasks.backup_instances_all',
-        'schedule': crontab(hour=22)
+        'schedule': crontab(minute=0, hour=20)
     },
     'report_routine_backups': {
         'task': 'atlas.tasks.report_routine_backups',
-        'schedule': crontab(hour=6)
+        'schedule': crontab(minute=0, hour=6)
     },
     'verify_statistics_updating': {
         'task': 'atlas.tasks.verify_statistics',
-        'schedule': crontab(hour=6),
+        'schedule': crontab(minute=0, hour=6),
     },
     # 'migrate_routing': {
     #     'task': 'atlas.tasks.migrate_routing',

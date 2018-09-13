a716d5d44a39d0851d03b4009115ce8d3b727cbf
James W Fuller <James.W.Fuller@Colorado.EDU>
2018-09-13 09:20:07 -0600

    Bump version.

--- atlas/config.py
+++ atlas/config.py
@@ -63,4 +63,4 @@ if ENVIRONMENT == 'local':
     # https://urllib3.readthedocs.io/en/latest/advanced-usage.html#ssl-warnings
     urllib3.disable_warnings()
 
-VERSION_NUMBER = '2.2.2'
+VERSION_NUMBER = '2.3.0-alpha1'

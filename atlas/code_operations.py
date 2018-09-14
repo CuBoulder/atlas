afcf0277ba6d86121460bb97a643d6560cfb2cd4
James W Fuller <James.W.Fuller@Colorado.EDU>
2018-09-14 13:24:38 -0600

    Move util functions

--- atlas/code_operations.py
+++ atlas/code_operations.py
@@ -10,6 +10,7 @@ import subprocess
 
 from git import Repo, Git
 
+from atlas import utilities
 from atlas.config import (ENVIRONMENT, CODE_ROOT, LOCAL_CODE_ROOT)
 from atlas.config_servers import (SERVERDEFS)
 
@@ -25,7 +26,7 @@ def repository_clone(item):
     :return:
     """
     log.info('Code | Clone | Item - %s', item)
-    code_dir = code_path(item)
+    code_dir = utilities.code_path(item)
     # Clone repo
     if os.path.exists(code_dir):
         raise Exception('Destinaton directory already exists')
@@ -43,10 +44,10 @@ def repository_checkout(item):
     """
     log.info('Code | Checkout | Item - %s', item)
     # Fetch repo
-    repo = Repo(code_path(item))
+    repo = Repo(utilities.code_path(item))
     repo.remote().fetch()
     # Checkout commit
-    g = Git(code_path(item))
+    g = Git(utilities.code_path(item))
     g.checkout(item['commit_hash'])
 
 
@@ -58,7 +59,7 @@ def repository_remove(item):
     :return:
     """
     log.info('Code | Remove | Item - %s', item)
-    shutil.rmtree(code_path(item))
+    shutil.rmtree(utilities.code_path(item))
 
 
 def update_symlink_current(item):
@@ -67,12 +68,12 @@ def update_symlink_current(item):
     """
     code_folder_current = '{0}/{1}/{2}/{2}-current'.format(
         LOCAL_CODE_ROOT,
-        code_type_directory_name(item['meta']['code_type']),
+        utilities.code_type_directory_name(item['meta']['code_type']),
         item['meta']['name'])
     # Remove symlink if it exists
     if os.path.exists(code_folder_current):
         os.unlink(code_folder_current)
-    os.symlink(code_path(item), code_folder_current)
+    os.symlink(utilities.code_path(item), code_folder_current)
     log.debug('Code deploy | Symlink | %s', code_folder_current)
 
 
@@ -98,31 +99,3 @@ def sync_code():
         output = subprocess.check_output(cmd, shell=True)
         # TODO Catch exception for file permissions on target
         log.info('Code | Sync | %s', output)
-
-
-##
-# Utility functions for code operations
-##
-def code_path(item):
-    """
-    Determine the path for a code item
-    """
-    code_dir = '{0}/{1}/{2}/{2}-{3}'.format(
-        LOCAL_CODE_ROOT,
-        code_type_directory_name(item['meta']['code_type']),
-        item['meta']['name'],
-        item['meta']['version']
-    )
-    return code_dir
-
-
-def code_type_directory_name(code_type):
-    """
-    Determine the path for a code item
-    """
-    if code_type == 'library':
-        return 'libraries'
-    elif code_type == 'static':
-        return 'static'
-
-    return code_type + 's'
--- atlas/utilities.py
+++ atlas/utilities.py
@@ -322,6 +322,32 @@ def get_current_code(name, code_type):
         return False
 
 
+## Code related utility fucntions
+def code_path(item):
+    """
+    Determine the path for a code item
+    """
+    code_dir = '{0}/{1}/{2}/{2}-{3}'.format(
+        LOCAL_CODE_ROOT,
+        code_type_directory_name(item['meta']['code_type']),
+        item['meta']['name'],
+        item['meta']['version']
+    )
+    return code_dir
+
+
+def code_type_directory_name(code_type):
+    """
+    Determine the path for a code item
+    """
+    if code_type == 'library':
+        return 'libraries'
+    elif code_type == 'static':
+        return 'static'
+
+    return code_type + 's'
+
+
 def get_code(name, code_type=''):
     """
     Get the code item(s) for a given name and code_type.

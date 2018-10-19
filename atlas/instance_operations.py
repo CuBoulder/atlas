0dabc8b1c2cc1e87b2aa7a9a79df5775b520b1fe
James W Fuller <James.W.Fuller@Colorado.EDU>
2018-10-19 08:28:13 -0600

    Revert "Add short circut in order to prevent instance sync before we can confirm it. Add deployemnt notes with commands to backup and restore directories on web servers."
    This reverts commit 482334d0835be768ea1139695e7174d29127d81b.

--- atlas/instance_operations.py
+++ atlas/instance_operations.py
@@ -416,11 +416,10 @@ def sync_instances():
     """Copy the instance files to all of the relevant nodes.
     """
     log.info('Instances | Sync')
-    log.info('Instances | Short circuit')
-    # hosts = SERVERDEFS[ENVIRONMENT]['webservers'] + SERVERDEFS[ENVIRONMENT]['operations_server']
-    # # Sync INSTANCE_ROOT then WEB_ROOT
-    # for root in [tuple([LOCAL_INSTANCE_ROOT, INSTANCE_ROOT]), tuple([LOCAL_WEB_ROOT, WEB_ROOT])]:
-    #     utilities.sync(root[0], hosts, root[1], exclude='opcache')
+    hosts = SERVERDEFS[ENVIRONMENT]['webservers'] + SERVERDEFS[ENVIRONMENT]['operations_server']
+    # Sync INSTANCE_ROOT then WEB_ROOT
+    for root in [tuple([LOCAL_INSTANCE_ROOT, INSTANCE_ROOT]), tuple([LOCAL_WEB_ROOT, WEB_ROOT])]:
+        utilities.sync(root[0], hosts, root[1], exclude='opcache')
 
 
 def switch_web_root_symlinks(instance):

b5d8741a3c38d3cfdbc29a71fd021f27937affde
Jesus Gabriel Ortiz Tovar <jeor0980@colorado.edu>
2021-04-29 13:10:37 -0600

    Adding missing quotes to variable in setting.php

--- atlas/templates/settings.php
+++ atlas/templates/settings.php
@@ -26,7 +26,7 @@ $conf["atlas_statistics_id"] = "{{atlas_statistics_id}}";
 $conf["atlas_logging_url"] = "{{atlas_logging_url|join(sid)}}";
 
 // Express Site Metrics
-$conf['express_site_metrics_secret'] = "{{express_site_metrics_secret}}";
+$conf["express_site_metrics_secret"] = "{{express_site_metrics_secret}}";
 $path = "{{path}}";
 {% if status in ['launched', 'launching'] -%}
 $launched = TRUE;
